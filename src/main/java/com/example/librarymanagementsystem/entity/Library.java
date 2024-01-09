24264f5f80311160fa14bbf500d0f68a62a1522b
chandrika <chandrika.g@thrymr.net>
2024-01-09 15:23:22 +0530

    basic structure of project

---
+++ src/main/java/com/example/librarymanagementsystem/entity/Library.java
@@ -0,0 +1,21 @@
+package com.example.librarymanagementsystem.entity;
+
+import jakarta.persistence.Entity;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+/**
+ * @author chandrika.g
+ * user
+ * @ProjectName library-management-system
+ * @since 09-01-2024
+ */
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class Library {
+}
---
+++ src/main/java/com/example/librarymanagementsystem/service/UserService.java
@@ -0,0 +1,10 @@
+package com.example.librarymanagementsystem.service;
+
+/**
+ * @author chandrika.g
+ * user
+ * @ProjectName library-management-system
+ * @since 09-01-2024
+ */
+public class UserService {
+}
